eac08f2c34b60231f9633d4adc048c6113bf1453
Ralph Saladino <130468937+hplar05@users.noreply.github.com>
2024-10-13 04:19:05 +0800

    feat: folder structure (on progress)

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "../globals.css";
+
+const geistSans = localFont({
+  src: "../fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "../fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Admin - Philippine Public Service Leadership Awards",
+  description:
+    "Philippine Public Service Leadership Awards is honor individuals who make outstanding contributions and whos  accomplished are models of exemplary public service for those dedicated to the public good-now and in the future.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
