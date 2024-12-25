8974f5c40f6f2ffb775ed4899936428b4fa9a0f8
Ralph Saladino <130468937+hplar05@users.noreply.github.com>
2024-12-26 01:32:05 +0800

    feat: admin side initial setup

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,52 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <Card className="w-full max-w-md">
+        <CardHeader className="space-y-1">
+          <CardTitle className="text-2xl font-bold">Admin Login</CardTitle>
+          <CardDescription>
+            Enter your Admin email and password to the admin
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          <div className="space-y-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="admin@ppslawards.com"
+              required
+            />
+          </div>
+          <div className="space-y-2">
+            <Label htmlFor="password">Password</Label>
+            <Input
+              id="password"
+              type="password"
+              required
+              placeholder="admin password..."
+            />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Link className="w-full" href={"/admin/dashboard"}>
+            <Button className="w-full">Login</Button>
+          </Link>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
