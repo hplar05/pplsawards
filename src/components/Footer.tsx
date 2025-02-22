6e2db0336b2b74a5bc21312ee8a194bc34bd04be
Ralph Saladino <130468937+hplar05@users.noreply.github.com>
2025-02-22 14:42:19 +0800

    update: added border top

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import Logo from "/public/logo.png";
 
 export const Footer = () => {
   return (
-    <footer className="w-full py-6 bg-[#FEFEFE] text-black flex items-center justify-center">
+    <footer className="w-full py-6 bg-[#FEFEFE] text-black flex items-center justify-center border-t">
       <div className="container px-4 md:px-6 flex flex-col md:flex-row justify-between items-center">
         <div className="flex items-center space-x-2">
           <Image className="" src={Logo} alt="Logo" height={30} width={30} />
